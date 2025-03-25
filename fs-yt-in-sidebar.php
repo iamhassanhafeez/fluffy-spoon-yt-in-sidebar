c3039b2d4f46caca249bf483fa57ca14c397a04f
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-03-25 22:57:21 +0500

    Plugin basic has been setup

---
+++ fs-yt-in-sidebar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Fluffy Spoon YT in Sidebar
+ * Description: This plugin creates a widget to facilitate you with optins to show your youtube channel details in widebar or footer.
+ * Author: Hassan Hafeez
+ * Plugin URI: https://github.com/iamhassanhafeez/fluffy-spoon-yt-in-sidebar
+ * Author URI: https://github.com/iamhassanhafeez
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: fs-yt-in-sidebar
+ */
+
+ if(!defined('ABSPATH')) exit;
+
+ 
+?>
